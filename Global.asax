1a0b6821647d7d1c27d5fb55b7b8bb3147e4ee1a
smbolster <smbolster@gmail.com>
2014-01-30 18:58:43 -0500

    some classes modeled

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SoccerStats.WebApiApplication" Language="C#" %>
---
+++ Models/Team.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SoccerStats.Models
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        public string TeamName { get; set; }
+        public List<Player> Players { get; set; }
+
+    }
+}
