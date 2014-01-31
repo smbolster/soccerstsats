bc43f54e20f0b58340a3c077786a2bca917de65a
smbolster <smbolster@gmail.com>
2014-01-30 19:37:26 -0500

    full solution

---
+++ SoccerStats/Models/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SoccerStats.Models 
+{
+    public class Player : Person
+    {
+        public string Number { get; set; }
+        
+    }
+}
---
+++ SoccerStats/Models/Referee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SoccerStats.Models
+{
+    public class Referee : Person
+    {
+        public int RefId { get; set; }
+
+    }
+}
