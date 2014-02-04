d30ed7edeb09ee327dad1ed1910648a41d6f0692
smbolster <smbolster@gmail.com>
2014-02-04 13:24:13 -0500

    still re-arranging

--- Soccerstats.Data/Properties/AssemblyInfo.cs
+++ Soccerstats.Data/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Soccerstats.Data")]
+[assembly: AssemblyTitle("SoccerStats.Data")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Soccerstats.Data")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("SoccerStats.Data")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("37b0a7d6-c8d0-414e-89cf-9fd4227ec121")]
+[assembly: Guid("e7c8f222-4413-4961-9cd6-745393a34658")]
 
 // Version information for an assembly consists of the following four values:
 //
